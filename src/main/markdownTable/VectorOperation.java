c5894b606506e9d3b26d85cf4016eeb5ba7e1159
Noah George Michael <noah.michael@mni.thm.de>
2017-12-18 22:03:02 +0100

    Added JDoc for VectorOperation Interface

--- src/main/markdownTable/VectorOperation.java
+++ src/main/markdownTable/VectorOperation.java
@@ -1,5 +1,17 @@
 package main.markdownTable;
 
+@FunctionalInterface
 public interface VectorOperation {
+    /**
+     * Intended to be used with Lambda-Expressions.
+     * This function takes the two arguments v and index,
+     * a Vector representing a row or column from a table
+     * and an Integer representing the row or column number
+     * respectively. Implementations of this methos allow
+     * arbitrary manipulation of the supplied Vector.
+     *
+     * @param index the row or column index of the suppied vector
+     * @param v the supplied vector itself
+     */
     void manipulateVector(Integer index, Vector v);
 }
