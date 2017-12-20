d15ebf814460e2c095b28dc83088bde00a282f77
Noah George Michael <noah.michael@mni.thm.de>
2017-12-20 11:06:49 +0100

    Introduced a default implementation for the Cell interface

---
+++ src/main/MarkdownCell.java
@@ -0,0 +1,25 @@
+package main;
+
+import main.api.Cell;
+
+public class MarkdownCell implements Cell {
+
+    private String value;
+
+    MarkdownCell() {
+        value = "";
+    }
+    MarkdownCell(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
