e5d11ed450845ae9543a4e5b5b5bdf3e83df9f44
Noah George Michael <noah.michael@mni.thm.de>
2017-12-21 11:17:29 +0100

    Finished JavaDoc for Cell- and CellOperation-Interface

--- src/main/api/Cell.java
+++ src/main/api/Cell.java
@@ -1,14 +1,23 @@
 package main.api;
 
 /**
- * Describes a cell and its basic functionality from a MarkdownTable.
+ * Describes a cell and its basic functionality for a MarkdownTable.
  *
  * @author Noah George Michael <noah.michael@mni.thm.de>
  * @since 19.12.2017
  */
 public interface Cell {
 
+    /**
+     * Returns the value of this cell as a string
+     * @return the value
+     */
     String getValue();
+
+    /**
+     * Sets the value of this cell
+     * @param value the value for this cell
+     */
     void setValue(String value);
 
 }
--- src/main/api/CellOperation.java
+++ src/main/api/CellOperation.java
@@ -1,8 +1,30 @@
 package main.api;
 
+/**
+ * Functional Interface CellOperation
+ *
+ * This Interface describes a single non-default method
+ * for manipulating Cells of a Vector.
+ *
+ * @author Noah George Michael <noah.michael@mni.thm.de>
+ * @since 21.12.2017
+ */
 @FunctionalInterface
 public interface CellOperation {
 
+    /**
+     * This method is intended to be used with Lambda-Expressions.
+     *
+     * It executes the method described ba the Lambda-Expression
+     * with the supplied arguments. The aforementioned method can perform
+     * any arbitrary manipulation on the supplied cell.
+     *
+     *
+     * @param index the index of this cell in the parent vector
+     * @param cell the cell itself
+     * @see Cell
+     * @see Vector
+     */
     void manipulateCell(Integer index, Cell cell);
 
 }
