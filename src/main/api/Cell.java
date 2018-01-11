cb9997a19bf6eff267eebe6ac42f63526443a25e
Noah George Michael <noah.michael@mni.thm.de>
2018-01-12 00:23:26 +0100

    Introduced formulas to Cells

--- src/main/api/Cell.java
+++ src/main/api/Cell.java
@@ -11,13 +11,29 @@ public interface Cell {
     /**
      * Returns the value of this cell as a string
      * @return the value
+     * @since Version 1
      */
     String getValue();
 
     /**
      * Sets the value of this cell
      * @param value the value for this cell
+     * @since Version 1
      */
     void setValue(String value);
 
+    /**
+     * Replaces this cells value with a formula.
+     *
+     * @param formula the formula for calculating the value of this cell
+     * @since Version 2
+     */
+    void setFormula(Formula formula);
+
+    /**
+     * Returns the formula of this cell
+     * @return a Formula-Instance or null if this cell doesn't have a formula
+     * @since Version 2
+     */
+    Formula getFormula();
 }
--- src/main/markdownExcel/MarkdownCell.java
+++ src/main/markdownExcel/MarkdownCell.java
@@ -1,13 +1,22 @@
 package main.markdownExcel;
 
 import main.api.Cell;
+import main.api.Formula;
+import main.api.TableBuilder;
 
 public class MarkdownCell implements Cell {
 
     private String value;
+    private Formula formula;
+    private boolean hasFormula;
+
+    private int index;
+    private TableBuilder builderInstance;
 
     MarkdownCell() {
         value = "";
+        hasFormula = false;
+        formula = null;
     }
     MarkdownCell(String value) {
         this.value = value;
@@ -15,11 +24,26 @@ public class MarkdownCell implements Cell {
 
     @Override
     public String getValue() {
+        if (!hasFormula)
+            return value;
+        value = formula.execute(index, this, builderInstance);
         return value;
     }
 
     @Override
     public void setValue(String value) {
         this.value = value;
+        hasFormula = false;
+    }
+
+    @Override
+    public void setFormula(Formula formula) {
+        this.formula = formula;
+        this.hasFormula = true;
+    }
+
+    @Override
+    public Formula getFormula() {
+        return this.hasFormula ? formula : null;
     }
 }
